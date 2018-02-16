37137247fffcd9190cf00cc62d21b1bd9fa3f19e
篠原七子 <Nanako@shinoharananako-no-MacBook-Air.local>
2018-02-16 22:14:52 +0900

    画像のコピー

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    
+    var dispImageNo = 0
 
     @IBAction func onPrev(_ sender: Any) {
        
@@ -24,11 +26,20 @@ class ViewController: UIViewController {
         displayImage()
     }
     
-    var dispImageNo = 0
     
     func displayImage() {
         
-        let imageNameArray = ["images-3.jpeg", "images-2.jpeg", "5D853BF6-81A1-11DF-88B7-F2385CF189A3_l.jpg"]
+        let imageNameArray = ["5D853BF6-81A1-11DF-88B7-F2385CF189A3_l.jpg", "B98E3A38-80D4-11DF-AE89-F1385CF189A3_l.jpg", "e747989e649991156cc9303fc2edd339--sato.jpg"]
+        
+        if dispImageNo < 0 {
+            dispImageNo = 2
+        }
+        
+        // 範囲より上を指している場合、最初の画像を表示
+        if dispImageNo > 2 {
+            dispImageNo = 0
+            
+        }
         
         let name = imageNameArray[dispImageNo]
         
@@ -42,7 +53,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let image = UIImage(named: "5D853BF6-81A1-11DF-88B7-F2385CF189A3_l.jpg")
+        
+        
+        imageView.image = image
+        
     }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
